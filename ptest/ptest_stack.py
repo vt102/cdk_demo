428caed4ac2a418f8f678172fbfe54121dd8bcea
Andy Cowell <andycowe@amazon.com>
2021-08-29 19:26:23 +0000

    Testing build

--- ptest/ptest_stack.py
+++ ptest/ptest_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     core,
     core as cdk,
+    aws_codecommit as codecommit,
     pipelines,
 )
 
@@ -24,14 +25,25 @@ class PtestStack(cdk.Stack):
             self, 'PtestStackCodePipeline',
             synth=pipelines.ShellStep(
                 'Synth',
-                input=pipelines.CodePipelineSource.git_hub("vt102/cdk_demo", "main"),
+                input=pipelines.CodePipelineSource.code_commit(
+                    codecommit.Repository.from_repository_name(self, 'Repository', "cdk_demo"),
+                    branch='main'),
                 commands=[
-                    'npm install -g aws-cdk',
-                    'python -m venv .venv',
-                    '. .venv/bin/activate',
                     'pip install -r requirements.txt',
+                    'npm install -g aws-cdk',
+                    # 'python -m venv .venv',
+                    # '. .venv/bin/activate',
                     'cdk synth'
                 ]))
+
+        stg_build = p.add_stage(pipelines.Stage(self, 'TestBuild',
+                                                synth=pipelines.ShellStep(
+                                                    'SynthBuild',
+                                                    command=[
+                                                        'ls -al'
+                                                    ])))
+                                                    
+        
         stg_preprod = p.add_stage(EcsClusterStage(self, 'PreProd',
                                                   env=cdk.Environment(account="989957622819", region="us-east-2")),
                                   post=[pipelines.ManualApprovalStep('PreProd Acceptance')]
